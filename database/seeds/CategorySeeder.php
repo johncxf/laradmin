874aae86869e3df38138ac86a12106083a2468ea
johncxf <13777719513@163.com>
2020-02-12 14:40:12 +0800

    修改数据迁移文件

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -16,11 +16,20 @@ class CategorySeeder extends Seeder
         $CONN_DB = 'mysql_laradmin';
         $CATE_TB = 'category';
         DB::connection($CONN_DB)->table($CATE_TB)->insert([
-            'name' => '博客',
-            'alias' => 'blog',
-            'url' => 'https://blog.yiqiesuifeng.cn/',
-            'status' => 1,
-            'pid' => 0
+            [
+                'name' => '默认栏目',
+                'alias' => 'default',
+                'url' => '',
+                'status' => 0,
+                'pid' => 0
+            ],
+            [
+                'name' => '博客',
+                'alias' => 'blog',
+                'url' => 'https://blog.yiqiesuifeng.cn/',
+                'status' => 1,
+                'pid' => 0
+            ]
         ]);
     }
 }
